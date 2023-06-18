db23865c2ec5581b8ac81944f5ebac8d7631d678
siddhantprateek <siddhantprateek@gmail.com>
2023-06-19 00:26:59 +0530

    e2e server testing

--- cypress/e2e/api/route.cy.js
+++ cypress/e2e/api/route.cy.js
@@ -1,9 +1,20 @@
 /// <reference types="cypress" />
 
-describe('example to-do app', () => {
+describe('Polychain - Blockchain service', () => {
   beforeEach(() => {
+    // cy.server() // Start the server
+    cy.intercept({
+      method: 'GET',
+      url: '/',
+      response: { "message": 'Welcome to Polychain API.' },
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
     cy.visit('/')
   })
 
-
+  it('should display the welcome message', () => {
+    cy.contains(JSON.stringify({ message: 'Welcome to Polychain API.' })) // Check if the welcome message is displayed on the page
+  })
 })
