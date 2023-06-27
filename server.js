251c284cbf75ad89f847af6ebd27eb027a8bb435
siddhantprateek <siddhantprateek@gmail.com>
2023-06-27 17:46:20 +0530

    removed unwanted codes

--- server.js
+++ server.js
@@ -2,19 +2,15 @@ const express = require('express');
 const morgan  = require('morgan');
 const cors = require('cors');
 const contractRoutes = require('./routes/contract.routes');
+const accnRouter = require('./routes/account.routes');
 require('dotenv').config()
 
 const app = express()
-
 const PORT = process.env.PORT || 8009
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
-// app.use((req, res, next) => {
-//     console.log("🚀",req.body)
-//     next()
-// })
 
 app.get("/", (req, res) => {
     res.status(200).json({
@@ -23,6 +19,7 @@ app.get("/", (req, res) => {
 })
 
 app.use("/contract", contractRoutes)
+app.use("/account", accnRouter)
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
